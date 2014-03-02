b44da6bfef5b8735abc17cd3f2c80f483bd67ef7
Salmān Aljammāz <s@0x65.net>
2014-03-02 22:54:00 +0000

    stream from browser to browser

--- webcam.go
+++ webcam.go
@@ -5,24 +5,54 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"io"
+	"sync"
+	
+	"code.google.com/p/go.net/websocket"
 )
 
 var (
-	addr     = flag.String("addr", ":8003", "http address to listen on")
+	addr     = flag.String("addr", ":8888", "http address to listen on")
 	dataRoot = flag.String("data", "./ui", "data dir")
+
+	c, a *websocket.Conn
+	wg, done sync.WaitGroup
 )
 
-func stream(w http.ResponseWriter, r *http.Request) {
-	log.Println("Stream:", r.RemoteAddr, "connected")
-	w.Header().Add("Content-Type", "video/x-vnd.on2.vp8")
+func call(ws *websocket.Conn) {
+	log.Println(ws.Request().RemoteAddr, "connected")
+	defer log.Println(ws.Request().RemoteAddr, "disconnected")
 
-	streamVid(w)
-	log.Println("Stream:", r.RemoteAddr, "disconnected")
+	c = ws
+	wg.Done()
+	wg.Wait()
+	done.Add(1)
+	log.Println("copying")
+	io.Copy(a, c)
+	wg.Add(1)
+	done.Done()
+	done.Wait()
+}
+
+func answer(ws *websocket.Conn) {
+	log.Println(ws.Request().RemoteAddr, "connected")
+	defer log.Println(ws.Request().RemoteAddr, "disconnected")
+
+	a = ws
+	wg.Done()
+	wg.Wait()
+	done.Add(1)
+	log.Println("copying")
+	io.Copy(c, a)
+	done.Done()
+	done.Wait()
 }
 
 func main() {
 	flag.Parse()
-	http.HandleFunc("/cam", stream)
+	wg.Add(2)
+	http.Handle("/call", websocket.Handler(call))
+	http.Handle("/answer", websocket.Handler(answer))
 	http.Handle("/", http.FileServer(http.Dir(*dataRoot)))
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
