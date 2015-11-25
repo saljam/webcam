ff3c2c7272deb6f029dae80c4d9c0efd4c76583d
Salman Aljammaz <s@0x65.net>
2015-11-25 18:51:30 +0300

    remove chromium webrtc

--- peerconn.h
+++
@@ -1,6 +0,0 @@
-// Copyright 2014, Salman Aljammaz
-
-void init();
-void* Offer();
-void AddAnswer(void* pc, char* sdp); // add callback?
-void AddCandidate(void* pc, char* sdp,char* mid, int line);
