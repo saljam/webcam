8b2df2e881f8e3ad84c086610a099e293c1cd4dd
Salman Aljammaz <s@0x65.net>
2015-12-13 20:24:52 +0300

    cmd: simplify (remove websocket & webcomponents) and switch to new package

--- cmd/webcam/main.go
+++ cmd/webcam/main.go
@@ -3,6 +3,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -14,9 +16,29 @@ var (
 	dataRoot = flag.String("data", "./ui", "data dir")
 )
 
+func handleSession(w http.ResponseWriter, r *http.Request) {
+	offer, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("Couldn't read offer body: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	s := webcam.NewSession()
+	err = s.Remote(offer)
+	if err != nil {
+		log.Printf("Couldn't set remote description: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	answer, err := s.Description()
+	if err != nil {
+		log.Printf("Couldn't get local description: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	fmt.Fprintf(w, "%s", answer)
+}
+
 func main() {
 	flag.Parse()
-	http.HandleFunc("/cam/", webcam.ServeHTTP)
+	http.HandleFunc("/session", handleSession)
 	http.Handle("/", http.FileServer(http.Dir(*dataRoot)))
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
